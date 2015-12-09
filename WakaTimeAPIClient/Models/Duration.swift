e9755cc75dba387454d9728a25172fc280cf5d81
giginet <giginet.net@gmail.com>
2015-12-09 22:00:34 +0900

    Implement example API response

---
+++ WakaTimeAPIClient/Models/Duration.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Duration {
+    init?(dictionary: [String: AnyObject]) {
+
+    }
+}
--- WakaTimeAPIClient/WakaTimeRequestType.swift
+++ WakaTimeAPIClient/WakaTimeRequestType.swift
@@ -11,6 +11,10 @@ protocol WakaTimeRequestType: RequestType {
 }
 
 extension WakaTimeRequestType {
+    var method: HTTPMethod {
+        return .GET
+    }
+    
     var baseURL: NSURL {
         return NSURL(string: "https://wakatime.com/api/v1/")!
     }
@@ -19,12 +23,51 @@ extension WakaTimeRequestType {
         guard let apiKey = self.apiKey else {
             throw WakaTimeAPIClientError.APIKeyNotDefined
         }
-        
         guard let data = apiKey.dataUsingEncoding(NSUTF8StringEncoding) else {
             throw WakaTimeAPIClientError.AuthenticationFailure
         }
         let encryptedKey = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
         URLRequest.setValue("Basic \(encryptedKey)", forHTTPHeaderField: "Authorization")
         return URLRequest
+    }
+    
+    func errorFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) -> ErrorType? {
+        return nil
+    }
+}
+
+struct DurationRequest: WakaTimeRequestType {
+    typealias Response = Duration
+    let date: NSDate
+    var apiKey: String?
+    
+    init(date: NSDate) {
+        self.date = date
+    }
+    
+    var path: String {
+        return "/users/current/durations"
+    }
+    
+    var parameters: [String: AnyObject] {
+        let dateString = self.dateStringFromDate(self.date)
+        return ["date": dateString]
+    }
+    
+    private func dateStringFromDate(date: NSDate) -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter.stringFromDate(date)
+    }
+    
+    func responseFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) -> Response? {
+        guard let dictionary = object as? [String: AnyObject] else {
+            return nil
+        }
+        
+        guard let duration = Duration(dictionary: dictionary) else {
+            return nil
+        }
+        return duration
     }
 }
